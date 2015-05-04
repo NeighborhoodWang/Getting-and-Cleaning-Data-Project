ba7e950355caba3ebd5bc26d38e5a4427fd685fc
Assemble <xjgzhch@hotmail.com>
2015-05-04 21:59:50 +0800

    Update run_analysis.R
    New week hahaha!

--- run_analysis.R
+++ run_analysis.R
@@ -1,43 +1,61 @@
+## This script is for tide UCI HAR Dataset and make it easy to use
+## We only handle the column that is certained about mean and std
+
+## Step 0:load nessary packages
 library(dplyr)
 library(tidyr)
-x_train <- read.fwf(file="X_train.txt",skip=0,widths=rep(16,each=561))#load in x_train.txt
-features <- read.table(file="features.txt",sep=" ")#load in features.txt
+
+## Step 1:Load All necessary data
+
+x_train <- read.fwf(file=".\\UCI HAR Dataset\\train\\X_train.txt",skip=0,widths=rep(16,each=561))
+y_train <- read.table(file=".\\UCI HAR Dataset\\train\\y_train.txt",sep="\n")#load in y_train.txt
+subject_train <- read.table(file=".\\UCI HAR Dataset\\train\\subject_train.txt",sep="\n")
+
+x_test <- read.fwf(file=".\\UCI HAR Dataset\\test\\X_test.txt",skip=0,widths=rep(16,each=561))
+y_test <- read.table(file=".\\UCI HAR Dataset\\test\\y_test.txt",sep="\n")#load in y_test.txt
+subject_test <- read.table(file=".\\UCI HAR Dataset\\test\\subject_test.txt",sep="\n")#load in subject_test.txt
+
+features <- read.table(file=".\\UCI HAR Dataset\\features.txt",sep=" ")#load in features.txt
+activity_labels <- read.table(file=".\\UCI HAR Dataset\\activity_labels.txt",sep=" ")#load in activity_labels.txt
+
+## Step 2:Suset X_train.txt and X_test.txt with only “mean” and “std” columns
 meanIndex <- grep("mean",as.character(features$V2))#filter the index whose value has "mean"
 stdIndex <- grep("std",as.character(features$V2))#filter the index whose value has "std"
 meanAndStdIndex <- c(meanIndex,stdIndex)#bind the "mean index" and "std index" together
+x_trainMeanStd<-x_train[,meanAndStdIndex]#filter the dataset only the column is "mean" or "std"
+x_testMeanStd<-x_test[,meanAndStdIndex]#filter the dataset only the column is "mean" or "std"
+
+## Step 3:Get the descriptive(names) of every columns,Then tide the descriptive names.
 meanAndStdFeatures <- as.character(features$V2[meanAndStdIndex])#fetch the descriptive(names) of every columns
 descriptiveNames <- c(meanAndStdFeatures,"Activities","Subjects")#set up a vector full of descriptive(names)
-x_trainMeanStd<-x_train[,meanAndStdIndex]#filter the dataset only the column is "mean" or "std"
-x_test <- read.fwf(file="X_test.txt",skip=0,widths=rep(16,each=561))#load in x_test.txt
-x_testMeanStd<-x_test[,meanAndStdIndex]#filter the dataset only the column is "mean" or "std"
-y_train <- read.table(file="y_train.txt",sep="\n")#load in y_train.txt
-y_test <- read.table(file="y_test.txt",sep="\n")#load in y_test.txt
-subject_test <- read.table(file="subject_test.txt",sep="\n")#load in subject_test.txt
-subject_train <- read.table(file="subject_train.txt",sep="\n")#load in subject_train.txt
-activity_labels <- read.table(file="activity_labels.txt",sep=" ")#load in activity_labels.txt
+
+## Step 4:Column bind train with activity and subject
+##        Column bind test with activity and subject
 train <- cbind(x_trainMeanStd,y_train,subject_train)#bind the xTrain,yTrain,subjuctTrain together
 test <- cbind(x_testMeanStd,y_test,subject_test)#bind the xTest,yTest,subjuctTest together
+
+## Step 5:Row bind train,test to “dataset”
 dataset <- rbind(train,test)#bind trainSet and testSet
+
+## Step6:Tide the data set
+## Assign the descriptive names to the column names of dataset
 names(dataset) <- descriptiveNames#assign the descriptive to the columnNames
-averageSub <- dataset[180,]#set up a Pseudo-Zero data.frame,init this data.frame for later use
-for(j in 1:30)#the index of Subjects
-{
-    for(k in 1:6)#the index of Activities
-  {
-    aveRowIndex <- (j-1)*6 + k
-    temp <- filter(dataset,Activities==k,Subjects==j)
-    for(l in 1:79)#the index of columns
-    {
-      averageSub[aveRowIndex,l] <- mean(temp[,l])#get the mean of specified column,subject and activities
-    }
-    averageSub[aveRowIndex,80] <- k
-    averageSub[aveRowIndex,81] <- j
-  }
-}
+
+## Step7:Aggregate the “dataset” and get the average dataset that this assignment asked for.
+attach(dataset)
+a <- aggregate(dataset, by = list(Subjects, Activities), FUN = mean)
+averageSub <- subset(a, select=-c(Group.1, Group.2))
+
+## Step8 Tide the data set
+## Assign the activities numbers to explicitly descriptive.
 for(i in 1:6){
   dataset$Activities[dataset$Activities == i] <- as.character(activity_labels$V2[i])#make Activities easy to understand
-                              #change numbers to explicity descriptive
+  #change numbers to explicity descriptive
 }
 for(i in 1:6){
-  averageSub$Activities[averageSub$Activities == i] <- as.character(activity_labels$V2[i])
+  averageSub$Activities[averageSub$Activities == i] <- as.character(activity_labels$V2[i])#make Activities easy to understand
+  #change numbers to explicity descriptive
 }
+
+##Step 9:## Export this dataset as averageSub.txt
+write.table(averageSub, file = ".\\averageSub.txt", row.name=FALSE) 
